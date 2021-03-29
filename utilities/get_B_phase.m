9a5269d0b981e34b8bfca43140ae33b7b6ecb25d
CuicuiZhao2018 <you@example.com>
2021-03-29 13:38:57 +0800

    3/2021

--- utilities/get_B_phase.m
+++
@@ -1,7 +0,0 @@
-function B_phase = get_B_phase(B_mat)
-        
-%         B_phase = B_mat./abs(B_mat);
-%         B_phase(abs(B_mat)<1*10^-12) = 1;
-          B_phase = exp(sqrt(-1)*angle(B_mat));
-        
-end
--- utilities/mygradient.m
+++
@@ -1,4 +0,0 @@
-function gu=mygradient(u)
-gu=repmat(0,size(u));
-gu(1:end-1,:)=u(2:end,:)-u(1:end-1,:);
-end
